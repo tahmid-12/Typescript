d3b2e03bf45a82eddf030b98181fc1386e2c64c3
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-09-24 12:36:44 +0600

    Variable Type

---
+++ src/script.ts
@@ -0,0 +1,25 @@
+// var country = "Bangladesh";
+// const city = "Dhaka City";
+
+// console.log(country);
+// console.log(city);
+
+// Function
+// function multiply(a: number, b: number){
+//     return a * b;
+// }
+
+// console.log(multiply(5,9));
+
+
+// Array
+let mixed = ["apple",34, true,{}]
+
+mixed.push(55)
+mixed.push("sth")
+mixed.push(false)
+mixed.push({
+    name: "Tahmid"
+})
+
+console.log(mixed)
